cdbe41fb53607d4c6a451481ff0d080acde55543
Loris Tissino <loris.tissino@mattiussilab.net>
2012-05-31 16:31:29 +0200

    modified csharp example

--- csharp/BasicExample/Main.cs
+++ csharp/BasicExample/Main.cs
@@ -2,11 +2,33 @@ using System;
 
 namespace BasicExample
 {
+	class IncreasingNumber
+	{
+		private int v;
+		public IncreasingNumber()
+		{
+			v=0;
+		}
+		public void addValue(int x)
+		{
+			v+=x;
+		}
+		public int getValue()
+		{
+			return v;
+		}
+		
+	}
+	
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
 			int n=0;
+			
+			IncreasingNumber inum = new IncreasingNumber();
+			Console.WriteLine (string.Format("Valore corrente: {0}", inum.getValue()));
+			
 			Console.WriteLine ("Inserisci un numero:");
 			
 			string s;
@@ -25,6 +47,8 @@ namespace BasicExample
 				}
 			}
 			Console.WriteLine (string.Format("Numero inserito: {0}", n));
+			inum.addValue(n);
+			Console.WriteLine (string.Format("Valore corrente: {0}", inum.getValue()));
 		}
 	}
 }
