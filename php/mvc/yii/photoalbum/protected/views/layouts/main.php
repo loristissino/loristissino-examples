a114cb9d4d9ec57dfadc34dc564811e0efac0f0f
Loris Tissino <loris.tissino@gmail.com>
2013-01-06 11:02:12 +0100

    added license and fixed main views and menu

--- php/mvc/yii/photoalbum/protected/views/layouts/main.php
+++ php/mvc/yii/photoalbum/protected/views/layouts/main.php
@@ -30,6 +30,8 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
+				array('label'=>'Pictures', 'url'=>array('/picture/index')),
+				array('label'=>'Categories', 'url'=>array('/category/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
@@ -48,7 +50,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by Loris Tissino.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
--- php/mvc/yii/photoalbum/protected/views/site/pages/about.php
+++ php/mvc/yii/photoalbum/protected/views/site/pages/about.php
@@ -8,5 +8,22 @@ $this->breadcrumbs=array(
 ?>
 <h1>About</h1>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<p>This is a demo website aimed to illustrate how Yii framework works. It is not complete, since it is meant to be something to work on to build something better.</p>
+<p>The source code is available at <?php echo CHtml::link('Google Code', 'http://code.google.com/p/loristissino-examples/source/browse/php/mvc/yii/photoalbum/') ?>.</p>
+
+<h2>Author</h2>
+
+<p>WondefulPhotoAlbum &copy; 2012/13 <?php echo CHtml::link('Loris Tissino', 'http://loris.tissino.it') ?></p>
+
+<h2>Licence</h2>
+
+<p>This program is free software: you can redistribute it and/or modify
+it under the terms of the <?php echo CHtml::link('GNU General Public License', 'http://www.gnu.org/licenses/gpl.html') ?> as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.</p>
+
+<p>This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.</p>
+
